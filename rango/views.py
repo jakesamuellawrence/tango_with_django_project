2c74344f5798c727c4cefd538041c6ccc6693d6e
Jake Lawrence <Jake Lawrence>
2021-01-22 16:13:55 +0000

    Change minor semantics  - change small syntactical details in order to pass chapter 3 test

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.shortcuts import reverse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href=\"/rango/about\"> About </a>")
+    return HttpResponse("Rango says hey there partner! <a href=\"/rango/about/\">About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href=\" /rango/ \"> index </a>")
+    return HttpResponse("Rango says here is the about page. <a href=\"/rango/\">Index</a>")
