6249162520d3cbf80337e3dd1b1a552395073b2d
Jake Lawrence <Jake Lawrence>
2021-01-22 15:36:03 +0000

    Add About page  - add about view to rango  - map /about url to about view

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from rango import views
 
 app_name = 'rango'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
 ]
