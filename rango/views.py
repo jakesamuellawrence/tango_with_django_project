15f487377e3de92a0c34e559e84462ce697e156f
Jake Lawrence <Jake Lawrence>
2021-01-27 18:06:25 +0000

    Change index view to use template  - Set up TEMPLATE_DIR in settings and point settings to it  - Create index.html template

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-from django.shortcuts import reverse
+from django.shortcuts import render, HttpResponse, reverse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href=\"/rango/about/\">About</a>")
+    context = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context)
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href=\"/rango/\">Index</a>")
