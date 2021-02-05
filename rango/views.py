384299135d304bd78f72167b47f2fcde9ce244ac
Jake Lawrence <Jake Lawrence>
2021-02-05 10:48:56 +0000

    Chpt 6: Make index and category draw data to display from database

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render, HttpResponse, reverse
+from rango.models import Category, Page
 
 def index(request):
-    context = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/index.html', context=context)
+    category_list = Category.objects.order_by('-likes')[:5]
+    most_viewed_pages = Page.objects.order_by('-views')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = most_viewed_pages
+    return render(request, 'rango/index.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
+
 
 def about(request):
     return render(request, 'rango/about.html', context={})
