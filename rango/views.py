8382ee6e786b329cc3969a668763e60ccaac0827
Jake Lawrence <Jake Lawrence>
2021-02-11 11:39:16 +0000

    Chpt 10: Add session cookies for implementing visit counter

--- rango/views.py
+++ rango/views.py
@@ -1,15 +1,40 @@
 from django.contrib.auth.decorators import login_required
+from django.db import reset_queries
 from rango.forms import CategoryForm, PageForm
 from django.shortcuts import redirect, render, HttpResponse, reverse
 from rango.models import Category, Page
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                                  str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
 
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
 
 def index(request):
+    visitor_cookie_handler(request)
+
     category_list = Category.objects.order_by('-likes')[:5]
     most_viewed_pages = Page.objects.order_by('-views')[:5]
 
@@ -18,6 +43,15 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = most_viewed_pages
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    visitor_cookie_handler(request)
+
+    context_dict = {
+        'visits': request.session['visits']
+    }
+
+    return render(request, 'rango/about.html', context_dict)
 
 def register(request):
     registered = False # Did registration succeed
@@ -132,6 +166,3 @@ def add_page(request, category_name_slug):
 
     context_dict = {'form':form, 'category':category}
     return render(request, 'rango/add_page.html', context=context_dict)
-
-def about(request):
-    return render(request, 'rango/about.html', context={})
